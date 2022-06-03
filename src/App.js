2e1fc3f788ae93ed8acb76993f21c5317f44d26a
Christian Huelmo <chuelmo@gmail.com>
2022-06-02 22:04:28 -0300

    Primer entrega - creación del proyecto con create-react-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './carritoTransparente.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Comenzando el curso de React
+        </p>
+        <p>Primer entrega</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
