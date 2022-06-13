42ee3925b753083ed9bc39a35d6b15d4853b76b6
Christian Huelmo <chuelmo@gmail.com>
2022-06-13 01:08:39 -0300

    Crear una barra de menu simple con Material UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './carritoTransparente.png';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Comenzando el curso de React
-        </p>
-        <p>Primer entrega</p>
-      </header>
+      <NavBar />
+      <h2>Las ofertas de la semana</h2>
     </div>
   );
 }
